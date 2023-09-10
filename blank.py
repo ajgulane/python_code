cf38c9297e3e243561ca49090280a855082e1094
Jays <111653199+JaysGITH@users.noreply.github.com>
2023-09-10 23:26:44 +0800

    a

---
+++ blank.py
@@ -0,0 +1,11 @@
+alist = [3, 67, "cat", [56, 57, "dog"], [ ], 3.14, False]
+print(alist[5])
+
+
+alist = [1, 57, "Reid", 3.1416, False]
+print(len(alist))
+
+sentence = 'desiderata = “Speak your truth quietly and clearly; and listen to others, even to the dull and the ignorant; they too have their story."'
+excerpt_des = sentence[31:50]
+print(excerpt_des)
+print(excerpt_des[8:11])
