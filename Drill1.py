c3b9895d7df2681cecea5efcfa636d2c490e746e
Jays <111653199+JophilGulane@users.noreply.github.com>
2023-10-10 18:51:56 +0800

    a

--- Drill1.py
+++ Drill1.py
@@ -1,21 +1,41 @@
-#Create a program that will let the user choose to get voltage, resistance, and curruents
-def drill(userC):
-    if userC == 1:
-        i = float(input("Enter Currents Value: "))
-        r = float(input("Enter Resistance Value: "))
-        v = i * r
-        print("Voltage(V) is = ", v)
-    elif userC == 2:
-        v = float(input("Enter Volts Value: "))
-        r = float(input("Enter Resistance Value: "))
-        i = v / r
-        print("Current(A) is = ", i)
-    elif userC == 3:
-        v = float(input("Enter Volts Value: "))
-        i = float(input("Enter Current Value: "))
-        r = v * i
-        print("Resistance(R) is = ", r)
-    else:
-        print("invalid value")
+def calculate_voltage(current, resistance):
+    return current * resistance
 
-drill(int(input("Voltage(V) = 1\nCurrent(A) = 2\nResistance(R) = 3\nChoose Command: ")))
+def calculate_current(voltage, resistance):
+    return voltage / resistance
+
+def calculate_resistance(voltage, current):
+    return voltage / current
+
+while True:
+    print("Choose an option:")
+    print("1. Calculate Voltage")
+    print("2. Calculate Current")
+    print("3. Calculate Resistance")
+    print("4. Quit")
+
+    choice = input("Enter your choice 1,2,3,4: ")
+
+    if choice == '1':
+        current = float(input("Enter current: "))
+        resistance = float(input("Enter resistance: "))
+        voltage = calculate_voltage(current, resistance)
+        print(f"Voltage is {voltage}")
+
+    elif choice == '2':
+        voltage = float(input("Enter voltage: "))
+        resistance = float(input("Enter resistance: "))
+        current = calculate_current(voltage, resistance)
+        print(f"Current is {current}")
+
+    elif choice == '3':
+        voltage = float(input("Enter voltage: "))
+        current = float(input("Enter current: "))
+        resistance = calculate_resistance(voltage, current)
+        print(f"Resistance is {resistance}")
+
+    elif choice == '4':
+        break
+
+    else:
+        print("Invalid choice. Please choose a valid option (1/2/3/4)")
